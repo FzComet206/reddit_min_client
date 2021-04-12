3dad9516cb3f7f3a08abf85a3a3055c7bbe953f9
Michael Cao <caozibo2000@gmail.com>
2021-04-12 12:33:17 +0800

    boilerplate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
+import { Props } from 'framer-motion/types/types'
 
 import theme from '../theme'
 
-function MyApp({ Component, pageProps }) {
+interface IProps {
+  Component: React.FC<Props>
+  pageProps: React.FC<Props>
+}
+
+const MyApp: React.FunctionComponent<IProps> = ({ Component, pageProps }) => {
   return (
     <ChakraProvider resetCSS theme={theme}>
       <ColorModeProvider
