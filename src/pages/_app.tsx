6c13d9f50293f80d28e3478ec6983afc3066451a
Michael Cao <caozibo2000@gmail.com>
2021-04-25 02:54:34 +0800

    added auto cache update when login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,79 @@
-import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
-import { Props } from 'framer-motion/types/types'
-import { Provider, createClient } from "urql"
-import theme from '../theme'
+import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
+import { Props } from 'framer-motion/types/types';
+import { Provider, createClient, dedupExchange, fetchExchange } from 'urql';
+import { Cache, cacheExchange, QueryInput } from '@urql/exchange-graphcache';
+import theme from '../theme';
 
 import config from '../config';
+import { LoginMutation, MeDocument, MeQuery, RegisterMutation } from '../generated/graphql';
+
+function betterUpdateQuery<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, data => fn(result, data as any) as any);
+}
 
 const client = createClient({
   url: `${config.serverip}:${config.serverport}/graphql`,
+  exchanges: [dedupExchange, cacheExchange({
+    updates: {
+      Mutation: {
+        login: (_result, args, cache, info) => {
+          betterUpdateQuery<LoginMutation, MeQuery>(
+            cache,
+            { query: MeDocument },
+            _result,
+            ( result, query ) => {
+              if (result.login.errors) {
+                return query
+              } else {
+                return {
+                  me: result.login.user,
+                }
+              }
+            }
+          )
+        },
+
+        register: (_result, args, cache, info) => {
+          betterUpdateQuery<RegisterMutation, MeQuery>(
+            cache,
+            { query: MeDocument },
+            _result,
+            ( result, query ) => {
+              if (result.register.errors) {
+                return query
+              } else {
+                return {
+                  me: result.register.user,
+                }
+              }
+            }
+          )
+        },
+      },
+    },
+  }), fetchExchange],
+  
   fetchOptions: {
-    credentials: "include",
+    credentials: 'include',
   },
-})   // fetch options has to do with cookies
+  // 
+});
+
+// this will run whenever we login or register, updating the cache
+
+
+
+// const client = createClient({
+//   url: `${config.serverip}:${config.serverport}/graphql`,
+//   fetchOptions: {
+//     credentials: "include",
+//   },
+// })   // fetch options has to do with cookies
 
 interface IProps {
   Component: React.FC<Props>
